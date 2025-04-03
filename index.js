2fe3085f9e29c686dee328a55e64db9e738ec9e2
Seio924 <seio9241@gmail.com>
2025-04-03 16:01:08 +0900

    Fix: 개발환경과 배포환경 환경변수 설정 분리

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const { Client, GatewayIntentBits, Events } = require('discord.js');
 const { startReminder, stopReminder } = require('./reminderManager');
