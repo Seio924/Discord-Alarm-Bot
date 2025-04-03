864bcd1cc5c612df681afe16539139881b7e8737
Seio924 <seio9241@gmail.com>
2025-04-03 16:10:31 +0900

    Fix: 환경변수 문제 해결 - 콘솔 제거

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-console.log('NODE_ENV:', process.env.NODE_ENV);
-console.log('TOKEN:', process.env.TOKEN);
-
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
@@ -9,7 +6,7 @@ const { Client, GatewayIntentBits, Events } = require('discord.js');
 const { startReminder, stopReminder } = require('./reminderManager');
 
 const token = process.env.TOKEN;
-console.log(token);
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
